cbd7886c7bae9374f34bf9b889a65e5c126c060f
suarezt3 <ey_der19@hotmail.com>
2021-10-13 15:20:42 -0500

    Se agregan nuevos modelos

--- API/Supermarket/api/models.py
+++ API/Supermarket/api/models.py
@@ -8,3 +8,34 @@ class Products (models.Model):
     date = models.DateField()
     description = models.CharField(max_length=250)
     category = models.CharField(max_length=60)
+
+
+class Supplier (models.Model):
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    telephone = models.PositiveIntegerField()
+    total_products = models.PositiveIntegerField()
+
+employee_position = [(1, 'Empleado'), (2, 'Administrador')]
+
+class Employee (models.Model):
+    name = models.CharField(max_length=100)
+    surname = models.CharField(max_length=100)
+    telephone = models.PositiveIntegerField()
+    position = models.IntegerField(
+        null=False, blank=False,
+        choices = employee_position,
+        default = 1
+    )
+
+class Orders (models.Model):
+    number_client = models.PositiveIntegerField()
+    shipping_address = models.CharField(max_length=100)
+    order_date = models.DateField()
+    number_article = models.PositiveIntegerField()
+    amount = models.PositiveIntegerField()
+
+
+
+
+
--- API/Supermarket/api/views.py
+++ API/Supermarket/api/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+#from django.http.response import JsonResponse
+#from django.shortcuts import render
+from django.views import View
+
+#from .models import Products
+
 
 # Create your views here.
+"""
+class ProductsView(View):
+
+    def get(self, request):
+        products = list(Products.objects.values())
+        if len(products)>0:
+            datos={'message':"Success", 'products':products}
+        else:
+            datos={'message':"Products not found..."}
+            return JsonResponse    
+
+    def post(self, request):
+        pass
+
+    def put(self, request):
+        pass
+
+    def delete(self, request):
+        pass
+
+"""
